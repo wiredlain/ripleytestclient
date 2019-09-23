6859f49dc9043fe76349c54abd4f23cbb194e149
David Navarrete <david.navarrete@2brains.cl>
2019-09-23 14:51:03 -0300

    se agregan dependencias

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/ripleytestclient'));
+app.use(express.static(__dirname + '/dist/client'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
