8e71682f3a49352f6e09cd765cc0c919531bbbee
David Navarrete <david.navarrete@2brains.cl>
2019-09-23 14:25:05 -0300

    se agregan dependencias

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
